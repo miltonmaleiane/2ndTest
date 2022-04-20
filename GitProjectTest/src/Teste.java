7f828f9402626ef29764e5198243e57c68a72766
miltonmaleiane <52857132+miltonmaleiane@users.noreply.github.com>
2022-04-20 14:19:59 +0200

    Update Teste.java

--- GitProjectTest/src/Teste.java
+++ GitProjectTest/src/Teste.java
@@ -2,5 +2,10 @@
 public class Teste {
 public static void main (String [] args) {
 	System.out.print("Hello folks");
+	int a,b;
+	a= 4;
+	b = 5;
+	System.out.print("Soma de"+a+ "e"+ b+ "="+ (a+b));
+	System.out.print("alterado pelo github");
 }
 }
